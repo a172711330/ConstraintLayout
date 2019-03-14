89ddc34fec90f82247772fd4ee84408891128b41
a172711330 <a172711330@gmail.com>
2019-03-14 13:05:47 +0800

    hw1

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int size = 30;
+
+    TextView hello;
+
+
+    public void bigger(View v)
+    {
+        hello.findViewById(R.id.textView);
+
+        hello.setTextSize(++size);
     }
 }
